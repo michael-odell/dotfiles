f33ea52a828ccb0f2c265492a71cc3f7b4d29d4d
Michael Odell <michael@theodells.us>
2025-02-14 13:34:59 -0700

    Open internal links in chrome.

--- .finicky.js
+++ .finicky.js
@@ -11,9 +11,7 @@ module.exports = {
             match: [
                 /(jamboard|analytics|meet|chrome|calendar|mail|docs|cloud|console.cloud|drive|groups|sites)\.google\.com(\/.*)?/,
                 /gmail\.com(\/.*)?/,
-                /insperity.com(\/.*)?/,
                 /ceph.odell.sh(\/.*)?/,
-                /lucid.app(\/.*)?/,
             ],
             // I always see this and want to use it, but it seems to just
             // match the raw domain, not the domain followed by any
@@ -26,6 +24,19 @@ module.exports = {
             browser: "Google Chrome"
         },
 
+        // Workday stuff in Chrome, too
+        {
+            match: [
+                /lucid.app(\/.*)?/,
+                /miro.com(\/.*)?/,
+                /ghe.megaleo.com/,
+                /(my)?workday.com(\/.*)?/,
+                /workdayinternal.com(\/.*)?/,
+                /workday.okta.com(\/.*)?/,
+            ],
+            browser: "Google Chrome"
+        },
+
         // Zoom join links open zoom directly, rather than first opening
         // a safari tab
         {
@@ -33,11 +44,111 @@ module.exports = {
             browser: "us.zoom.xos"
         },
 
+        // Slack links open slack directly
+        //{
+        //    match: [
+        //        /https:\/\/workday.enterprise.slack.com\/archives\/.*/,
+        //        /https:\/\/workday-dev.slack.com\/archives\/.*/,
+        //    ],
+        //    browser: "/Applications/Slack.app"
+        //},
+
+        {
+            match: ({ url }) => url.protocol === "slack",
+            browser: "/Applications/Slack.app"
+        },
+
         // NOTE: Thanks to folks on this thread, although this is my own, simpler solution.
         // See https://github.com/johnste/finicky/discussions/227
         {
             match: ({ url }) => url.host === "teams.microsoft.com",
             browser: "Microsoft Teams"
+        }
+    ],
+
+    rewrite: [
+        {
+            match: [
+                '*.slack.com/*',
+            ],
+            url: function({ url, urlString }) {
+                const subdomain = url.host.slice(0, -10)
+                const pathParts = url.pathname.split("/")
+
+                let team, patterns = {}
+                if (subdomain != 'app') {
+                    switch (subdomain) {
+                        case '<teamname>':
+                        case '<corpname>.enterprise':
+                            team = 'T00000000'
+                            break
+                        default:
+                            finicky.notify(
+                                `No Slack team ID found for ${url.host}`,
+                                `Add the team ID to ~/.finicky.js to allow direct linking to Slack.`
+                            )
+                            return url
+                    }
+
+                    if (subdomain.slice(-11) == '.enterprise') {
+                        patterns = {
+                            'file': [/\/files\/\w+\/(?<id>\w+)/]
+                        }
+                    } else {
+                        patterns = {
+                            'file': [/\/messages\/\w+\/files\/(?<id>\w+)/],
+                            'team': [/(?:\/messages\/\w+)?\/team\/(?<id>\w+)/],
+                            'channel': [/\/(?:messages|archives)\/(?<id>\w+)(?:\/(?<message>p\d+))?/]
+                        }
+                    }
+                } else {
+                    patterns = {
+                        'file': [
+                            /\/client\/(?<team>\w+)\/\w+\/files\/(?<id>\w+)/,
+                            /\/docs\/(?<team>\w+)\/(?<id>\w+)/
+                        ],
+                        'team': [/\/client\/(?<team>\w+)\/\w+\/user_profile\/(?<id>\w+)/],
+                        'channel': [/\/client\/(?<team>\w+)\/(?<id>\w+)(?:\/(?<message>[\d.]+))?/]
+                    }
+                }
+
+                for (let [host, host_patterns] of Object.entries(patterns)) {
+                    for (let pattern of host_patterns) {
+                        let match = pattern.exec(url.pathname)
+                        if (match) {
+                            let search = `team=${team || match.groups.team}`
+
+                            if (match.groups.id) {
+                                search += `&id=${match.groups.id}`
+                            }
+
+                            if (match.groups.message) {
+                                let message = match.groups.message
+                                if (message.charAt(0) == 'p') {
+                                    message = message.slice(1, 11) + '.' + message.slice(11)
+                                }
+                                search += `&message=${message}`
+                            }
+
+                            let output = {
+                                protocol: "slack",
+                                username: "",
+                                password: "",
+                                host: host,
+                                port: null,
+                                pathname: "",
+                                search: search,
+                                hash: ""
+                            }
+                            let outputStr = `${output.protocol}://${output.host}?${output.search}`
+                            finicky.log(`Rewrote Slack URL ${urlString} to deep link ${outputStr}`)
+                            return output
+                        }
+                    }
+                }
+
+                return url
+            }
         }
     ]
 };
